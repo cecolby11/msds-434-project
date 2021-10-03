a835814c09eb3794f6fa9721b2916c7488e0de43
Casey Colby <cecolby11@gmail.com>
2021-10-03 17:57:51 -0500

    rearrange directory structure

--- iac/modules/iam_role_app_engine.tf
+++ iac/modules/iam_role_app_engine.tf
@@ -9,9 +9,17 @@
 # }
 
 # per requirements here: https://cloud.google.com/appengine/docs/standard/python/roles
-resource "google_project_iam_member" "cicd_appengine" {
+resource "google_project_iam_member" "cicd_appengine_deployer" {
   project    = var.project_id
   role       = "roles/appengine.deployer"
+  member     = "serviceAccount:${google_service_account.cicd_deploy_gae.email}"
+}
+
+# Deployer not enough, contrary to documentation. Error you get is:
+# Your deployment has succeeded, but promoting the new version to default failed. You may not have permissions to change traffic splits. Changing traffic splits requires the Owner, Editor, App Engine Admin, or App Engine Service Admin role. 
+resource "google_project_iam_member" "cicd_appengine_admin" {
+  project    = var.project_id
+  role       = "roles/appengine.appAdmin"
   member     = "serviceAccount:${google_service_account.cicd_deploy_gae.email}"
 }
 
