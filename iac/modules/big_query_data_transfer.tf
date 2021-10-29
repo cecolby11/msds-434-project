6a1db0df1220c8edff5fdfee1a4b0e1b9a727bf4
Casey Colby <cecolby11@gmail.com>
2021-10-29 15:35:40 -0500

    Format

--- iac/modules/big_query_data_transfer.tf
+++ iac/modules/big_query_data_transfer.tf
@@ -2,11 +2,11 @@
 resource "google_bigquery_data_transfer_config" "query_config" {
   depends_on = [google_project_iam_member.data_transfer]
 
-  display_name         = "Generate Weekly Forecasts of Covid Cumulative Cases by State"
-  location             = "US"
-  data_source_id       = "scheduled_query"
-  service_account_name = google_service_account.bq_scheduled_query.name
-  schedule             = "every sunday 00:00"
+  display_name           = "Generate Weekly Forecasts of Covid Cumulative Cases by State"
+  location               = "US"
+  data_source_id         = "scheduled_query"
+  service_account_name   = google_service_account.bq_scheduled_query.name
+  schedule               = "every sunday 00:00"
   destination_dataset_id = google_bigquery_dataset.nyt.dataset_id
   params = {
     destination_table_name_template = google_bigquery_table.weekly_forecast_by_state.table_id
