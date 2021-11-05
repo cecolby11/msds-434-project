b741aef947d557d94a77e2aaec9dfc950d6d16df
Casey Colby <cecolby11@gmail.com>
2021-11-05 15:27:44 -0500

    sanitize state input in SQL query

--- gae_covid19/apis/gcp_big_query.js
+++ gae_covid19/apis/gcp_big_query.js
@@ -8,7 +8,7 @@ module.exports = {
         try {
             const forecastCumulativeCases = `SELECT forecast_timestamp, forecast_value
             FROM \`msds_434_project.nyt_weekly_forecast_by_state\`
-            WHERE LOWER(state_name) = '${state.toLowerCase()}' AND
+            WHERE LOWER(state_name) = @state AND
             forecast_timestamp > CURRENT_TIMESTAMP() AND forecast_timestamp < TIMESTAMP_ADD(CURRENT_TIMESTAMP, INTERVAL 7 DAY)`;
 
             // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
@@ -16,6 +16,7 @@ module.exports = {
                 query: forecastCumulativeCases,
                 // Location must match that of the dataset(s) referenced in the query.
                 location: 'US',
+                params: { state: state.toLowerCase() },
             };
         
             const [rows] = await bq.query(options);
