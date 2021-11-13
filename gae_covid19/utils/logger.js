f266e443561d71f7cb5bab609655179972bdcd88
Casey Colby <cecolby11@gmail.com>
2021-11-13 13:25:43 -0600

    rm error config

--- gae_covid19/utils/logger.js
+++ gae_covid19/utils/logger.js
@@ -5,12 +5,7 @@ const {
   // Imports the Google Cloud client library for Winston
 const {LoggingWinston} = require('@google-cloud/logging-winston');
 
-const loggingWinston = new LoggingWinston({
-    serviceContext: {
-      service: 'covid19', // required to auto-report logged errors to the Google Cloud Error Reporting console
-      version: '1.0'
-  }
-});
+const loggingWinston = new LoggingWinston();
 
   const {
     timestamp, json, combine, colorize, align, simple,
