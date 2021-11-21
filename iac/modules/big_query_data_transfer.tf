13c3d259f0463acbf88d3d8f105532e774a1cb69
Casey Colby <cecolby11@gmail.com>
2021-11-21 13:33:29 -0600

    schedule query

--- iac/modules/big_query_data_transfer.tf
+++ iac/modules/big_query_data_transfer.tf
@@ -9,11 +9,27 @@ resource "google_bigquery_data_transfer_config" "query_config" {
   location               = "US"
   data_source_id         = "scheduled_query"
   service_account_name   = google_service_account.bq_scheduled_query.email
-  schedule               = "every sunday 00:00"
+  schedule               = "every sunday 00:05"
   destination_dataset_id = google_bigquery_dataset.nyt.dataset_id
   params = {
     destination_table_name_template = google_bigquery_table.weekly_forecast_by_state.table_id # make sure there's underscores not dashes in this table's name
     write_disposition               = "WRITE_TRUNCATE" # vs WRITE_APPEND
     query                           = file("${path.module}/bq_ml_forecast_us_cases/bq_forecast_from_model.sql")
+  }
+}
+
+resource "google_bigquery_data_transfer_config" "model" {
+  depends_on = [google_project_iam_member.data_transfer, google_project_iam_member.data_transfer_bq_admin]
+
+  display_name           = "Model Covid Cumulative Cases by State"
+  location               = "US"
+  data_source_id         = "scheduled_query"
+  service_account_name   = google_service_account.bq_scheduled_query.email
+  schedule               = "every sunday 00:00"
+  destination_dataset_id = google_bigquery_dataset.nyt.dataset_id
+  params = {
+    # destination_table_name_template = google_bigquery_table.weekly_forecast_by_state.table_id # make sure there's underscores not dashes in this table's name
+    # write_disposition               = "WRITE_TRUNCATE" # vs WRITE_APPEND
+    query                           = file("${path.module}/bq_ml_forecast_us_cases/bq_create_model.sql")
   }
 }
